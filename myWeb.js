d3da50ec415f9905746f65f9735e2bb57d5da1a0
Hsiang <61619308+Arlyan-Hsiang@users.noreply.github.com>
2020-10-21 22:42:53 +1300

    Add files via upload

--- myWeb.js
+++ myWeb.js
@@ -1,27 +1,27 @@
 //For Size selection
-var buttonS = document.getElementById('S-size');
-var buttonM = document.getElementById('M-size');
-var buttonL = document.getElementById('L-size');
-var buttonOK = document.getElementById('comfirm');
+let buttonS = document.getElementById('S-size');
+let buttonM = document.getElementById('M-size');
+let buttonL = document.getElementById('L-size');
+let buttonOK = document.getElementById('comfirm');
 //for variable number
-var mySize = document.querySelector('table tr td i1');
-//mySize.textContent = '0';
-var myAmount = document.querySelector('h1 a1');
-//myAmount.textContent = '0';
+let mySize = document.querySelector('table tr td i1');
+let myAmount = document.querySelector('h1 a1');
 //for calculation
-var amount = 0;
-
+let amount = 0;
+//While click Size S
 buttonS.onclick = function() {
-    mySize.innerHTML = 'S';
-}
+        mySize.innerHTML = 'S';
+    }
+    //While click Size M
 buttonM.onclick = function() {
-    mySize.innerHTML = 'M';
+        mySize.innerHTML = 'M';
 
-}
-
+    }
+    //While click Size L
 buttonL.onclick = function() {
-    mySize.innerHTML = 'L';
-}
+        mySize.innerHTML = 'L';
+    }
+    //While click "Comfirm"
 buttonOK.onclick = function() {
     if (mySize.innerHTML == 'S' ||
         mySize.innerHTML == 'M' ||
